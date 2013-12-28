9bc6fb1ef01d3e6c343d691ffd48fe897d149d82
PavelK <2me.pavelk@gmail.com>
2013-12-29 02:54:02 +0400

    Опять

--- MZ_Export.php
+++ MZ_Export.php
@@ -115,10 +115,10 @@ class MZ_export {
  				$lot->addChild("id", $imgIndx);               
  				$title=$the_query->post->post_title;									
  				$descr=$the_query->post->post_content;     
- 				preg_match_all("/([а-яА-Яa-zA-ZёЁ\d\s\.\-\(\)\?\!\:,]+)/u", $descr, $rdescr, PREG_SET_ORDER); //-- берём только текст и нужные символы.       
+ 				preg_match_all("/([а-яА-Яa-zA-ZёЁ\d\s\.\-\(\)\?\!\:,]+)/u", $descr, $rdescr); //-- берём только текст и нужные символы.       
  				var_dump($rdescr[0]);
  				echo '<br>';
- 				var_dump($rdescr[0]);
+ 				var_dump($rdescr);
  				$lot->addChild("description", $rdescr[0] );				
  				$lot->addChild("title", $title);      
  				//--          
